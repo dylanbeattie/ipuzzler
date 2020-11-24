e1c734e6d8735268d491766be82795f5933c386c
Dylan Beattie <dylan@dylanbeattie.net>
2020-11-24 14:48:13 +0000

    Rendering support for barred puzzles

--- ipuzzler.js
+++ ipuzzler.js
@@ -1,42 +1,48 @@
-function iPuzzler(ipuz) {
+function iPuzzler(ipuz, $container) {
 
-    this.width = ipuz.dimensions.width;
-    this.height = ipuz.dimensions.height;
+    const $grid = $container.find("div.puzzle-grid");
 
-    this.render = function ($container) {
-        let $grid = $container.find("div.puzzle-grid");
-        this.layoutPuzzleGrid($grid);
-
-        this.drawPuzzle($grid);
+    this.ready = function () {    
+        puzzle.layoutPuzzleGrid();
+        puzzle.drawPuzzle();
+        puzzle.handleResize();
     }
 
     this.createGridCell = function (x, y, cell) {
-        var $span = $("<span class='cell'></span>");
+        let $span = $("<span class='cell'></span>");
+        if (cell.style && cell.style.barred) $span.addClass(`barred-${cell.style.barred.toLowerCase()}`);
+        if (cell.cell) cell = cell.cell;
         if (parseInt(cell)) $span.append(`<span class="clue-number">${cell}</span>`);
         if (cell == "#") {
             $span.addClass("block");
         } else {
-            $span.append(`<input maxlength='1' data-x="${x}" data-y="${y}" value="W" />`);
+            $span.append(`<input maxlength='1' data-x="${x}" data-y="${y}" value="" />`);
         }
         return ($span);
     }
-    this.drawPuzzle = function($grid) {
+    this.drawPuzzle = function () {
         ipuz.puzzle.forEach((row, y) => row.forEach((cell, x) => $grid.append(this.createGridCell(x, y, cell))));
     }
 
-    this.layoutPuzzleGrid = function ($grid) {
+    this.layoutPuzzleGrid = function () {
         $grid.html("");
-        let gridCss = { "grid-template": `repeat(${this.width}, 1fr) / repeat(${this.height}, 1fr)` }
+        let gridCss = { "grid-template": `repeat(${ipuz.dimensions.width}, 1fr) / repeat(${ipuz.dimensions.height}, 1fr)` }
         if ($(window).width() > 768) {
-            gridCss.width = `${this.width * 32}px`;
-            gridCss.height = `${this.height * 32}px`;
+            gridCss.width = `${ipuz.dimensions.width * 32}px`;
+            gridCss.height = `${ipuz.dimensions.height * 32}px`;
         }
-    
         $grid.css(gridCss);
-        let gridSize = $grid.width();
-        $grid.find("input").css("font-size", `${(Math.ceil(gridSize / (1.5 * this.height)))}px`);
-        $grid.find("span.clue-number").css("font-size", `${(Math.ceil(120 / this.height))}px`);
-
     }
+    
+    this.handleResize= function () {
+        let gridSize = $grid.width();
+        $grid.find("input").css("font-size", `${(Math.ceil(gridSize / (1.6 * ipuz.dimensions.height)))}px`);
+        $grid.find("span.clue-number").css("font-size", `${(Math.ceil(120 / ipuz.dimensions.height))}px`);
+    }
+
+    const puzzle = this;
+    // Attach handlers
+    $(window).resize(puzzle.handleResize);
+    $(puzzle.ready);
 }
 
